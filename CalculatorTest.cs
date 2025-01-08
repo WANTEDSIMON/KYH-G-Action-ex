4c7bd1947f9198467978957601d7415e971f64ed
Simon Major <simon.major@learnet.se>
2025-01-08 10:04:51 +0100

    first commit

---
+++ CalculatorTest.cs
@@ -0,0 +1,39 @@
+using Xunit;
+
+public class CalculatorTest
+{
+    [Fact]
+    public void TestAdd()
+    {
+        //Given
+        Calculator calc = new();
+        int a = 5;
+        int b = 2;
+        int expectedResult = 7;
+
+        //When
+        int result = calc.Add(a, b);
+
+        //Then
+        Assert.Equal(expectedResult, result);
+        
+    }
+
+    [Fact]
+    public void TestSubtract()
+    {
+        //Given
+        Calculator calc = new();
+        int a = 5;
+        int b = 2;
+        int expectedResult = 3;
+
+        //When
+        int result = calc.Subtract(a, b);
+
+        //Then
+        Assert.Equal(expectedResult, result);
+        
+    }
+
+}
